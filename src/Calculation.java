18a7456ff9b8ee9caef85c4671f1ded81e174741
doxie <doxie.zhang@gmail.com>
2015-04-11 14:13:13 +0800

    Find required request strings.

---
+++ src/Calculation.java
@@ -0,0 +1,9 @@
+
+public class Calculation {
+	String loginRequestString = //登录请求链接
+			"http://202.118.31.197/ACTIONLOGON.APPPROCESS?mode=&WebUserNO=20124730&Password=123456&Agnomen=z33j";
+	String queryRequstString =  //成绩查询链接
+			"http://202.118.31.197/ACTIONQUERYSTUDENTSCORE.APPPROCESS";
+	String verifyCodeRequestString = //验证码请求链接
+			"http://202.118.31.197/ACTIONVALIDATERANDOMPICTURE.APPPROCESS";
+}
