baed14f05e8dfc38ff3c65eeaf8cc8fc217c37b0
doxie <doxie.zhang@gmail.com>
2015-04-15 21:29:05 +0800

    建立课程model类

---
+++ src/Course.java
@@ -0,0 +1,61 @@
+
+public class Course {
+	private String property;
+	private String number;
+	private String name;
+	private String type;
+	private String time;
+	private String credit;
+	private String scoreType;
+	private int score;
+	
+	public String getProperty() {
+		return property;
+	}
+	public void setProperty(String property) {
+		this.property = property;
+	}
+	public String getNumber() {
+		return number;
+	}
+	public void setNumber(String number) {
+		this.number = number;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getTime() {
+		return time;
+	}
+	public void setTime(String time) {
+		this.time = time;
+	}
+	public String getCredit() {
+		return credit;
+	}
+	public void setCredit(String credit) {
+		this.credit = credit;
+	}
+	public String getScoreType() {
+		return scoreType;
+	}
+	public void setScoreType(String scoreType) {
+		this.scoreType = scoreType;
+	}
+	public int getScore() {
+		return score;
+	}
+	public void setScore(int score) {
+		this.score = score;
+	}
+	
+}
